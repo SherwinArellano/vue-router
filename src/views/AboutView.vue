27ee53f1c7bedf41d869ad5cdbd3e3a1325932f4
Sherwin Arellano <s.arellano@esis-italia.com>
2025-04-07 15:09:29 +0200

    Write basic example of routing

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,21 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-</template>
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+const router = useRouter()
+const route = useRoute()
+
+const search = computed({
+  get() {
+    return route.query.search ?? ''
+  },
+  set(search) {
+    router.replace({ query: { search } })
+  },
+})
+</script>
+
+<template>
+  <h2>AboutView</h2>
+  <label> Search: <input v-model.trim="search" maxlength="20" /> </label>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+function goToAbout() {
+  router.push('/about')
+}
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h2>HomeView</h2>
+  <button @click="goToAbout">Go to About</button>
 </template>
