aa6423e12676e7723b30c315e97e57007f623f9e
Sherwin Arellano <s.arellano@esis-italia.com>
2025-04-07 15:41:21 +0200

    Install and setup BootstrapVueNext

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
+import { VueQueryPlugin } from '@tanstack/vue-query'
 import { createApp } from 'vue'
+import { createBootstrap } from 'bootstrap-vue-next'
 import App from './App.vue'
 import router from './router'
-import { VueQueryPlugin } from '@tanstack/vue-query'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 
 const app = createApp(App)
 
 app.use(router)
 
 app.use(VueQueryPlugin)
+
+app.use(createBootstrap())
 
 app.mount('#app')
